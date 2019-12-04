60e0f1c0849d52310852319f7095f5075dad37b8
Alex Wilson <alex.david.wilson@gmail.com>
2019-12-04 08:33:37 -0700

    day_4 using BitVec instead of Int, orders faster

--- day_4/part2.py
+++ day_4/part2.py
@@ -4,9 +4,9 @@ import z3
 
 s = z3.Solver()
 
-pw = z3.Int('pw')
-
-digits = [z3.Int(f'd_{x}') for x in range(0, 6)]
+# Wow, BitVec is orders than Int, even with constraints on each variable
+pw = z3.BitVec('pw', 32)
+digits = [z3.BitVec(f'd_{x}', 32) for x in range(0, 6)]
 
 for x in range(0, 6):
     s.add(z3.And(digits[x] >= 0, digits[x] <= 9))
@@ -33,7 +33,7 @@ while True:
     if s.check() == z3.sat:
         num_passwords += 1
         m = s.model()
-        print(f'pw = {m[pw]}')
+        # print(f'pw = {m[pw]}')
         s.add(pw != m[pw])
     else:
         print(f'number of passwords: {num_passwords}')
